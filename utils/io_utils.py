0d1132af7671bf73eee497bb05b70106a70c1fb2
KallistaKomdeur <k.j.komdeur@student.tue.nl>
2026-01-19 19:56:00 +0100

    Comments for understandability

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -1,11 +1,17 @@
 import argparse
 
+# ======================
+# MAIN FUNCTION
+# ======================
 def get_input() -> tuple:
+    """
+    Parser to get variable values from input command. Some choices are fixed (see README). 
+    """
     parser = argparse.ArgumentParser(description="Run LLM experiment on event log")
     # Required
     parser.add_argument("log_name", type=str, help="Name of the event log")
     parser.add_argument("provider", type=str, choices=["gemini", "openai", "anthropic"], help="LLM provider")
-    parser.add_argument("configuration", type=str, choices=["single", "global_only", "inter-case_only"], help="Which input configuration to use")
+    parser.add_argument("configuration", type=str, choices=["single", "inter-case"], help="Which input configuration to use")
     # Optional
     parser.add_argument("--model", type=str, default = "2.5-flash", help="Model name for the provider (e.g. 2.5-flash)")
     args = parser.parse_args()
