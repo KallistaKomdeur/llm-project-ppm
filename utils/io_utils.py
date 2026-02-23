ea67ecfcdde05ae8de58c6ca4821bd86b54fe613
KallistaKomdeur <k.j.komdeur@student.tue.nl>
2026-02-24 00:59:41 +0100

    Support for LLM feature selection + with/without reasoning

--- utils/io_utils.py
+++ utils/io_utils.py
@@ -11,7 +11,7 @@ def get_input() -> tuple:
     # Required
     parser.add_argument("log_name", type=str, help="Name of the event log")
     parser.add_argument("provider", type=str, choices=["gemini", "openai", "anthropic"], help="LLM provider")
-    parser.add_argument("configuration", type=str, choices=["single_ref","inter-case_ref","single_split_ref","inter-case_split_ref", "single_exact"], help="Which input configuration to use")
+    parser.add_argument("configuration", type=str, choices=["single_ref","inter-case_ref","single_split_ref","inter-case_split_ref", "single_reasoning", "inter-case_self_select_split"], help="Which input configuration to use")
     # Optional
     parser.add_argument("--model", type=str, default = "2.5-flash", help="Model name for the provider (e.g. 2.5-flash)")
     args = parser.parse_args()
