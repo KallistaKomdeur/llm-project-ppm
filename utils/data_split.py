a13a2fefd41b0dc3ee5419a7a61ecfe6c974f432
KallistaKomdeur <k.j.komdeur@student.tue.nl>
2026-03-02 20:53:13 +0100

    Created alternative splitting method

--- utils/data_split.py
+++ utils/data_split.py
@@ -1,3 +1,5 @@
+import random
+
 def temporal_train_test_split(cases, train_ratio = 0.8):
     """
     Splits cases temporally assuming cases are already sorted by completion time (see paper)
@@ -25,6 +27,7 @@ def temporal_train_test_split(cases, train_ratio = 0.8):
         has_before = any(t <= t_split for t in times)
         has_after = any(t > t_split for t in times)
 
+        # Test cases must have events before and after t-split
         if has_before and has_after:
             # Keep only events up to t_split
             truncated_seq = [e for e, abs_t in zip(seq, times) if abs_t <= t_split]
@@ -36,3 +39,40 @@ def temporal_train_test_split(cases, train_ratio = 0.8):
             test_cases_trunc.append(truncated_case)
 
     return train_cases, test_cases_trunc
+
+def temporal_train_test_split_random_truncation(cases, train_ratio = 0.8, MIN_PREFIX_LENGTH = 2):
+    """
+    Splits cases temporally assuming cases are already sorted by completion time (see paper)
+    Train: first train_ratio of cases (= earliest completions).
+    Test: remaining 20% of cases, randomly truncated.
+    """
+
+    if not cases:
+        raise ValueError("Empty case list")
+    
+    # Assume cases are sorted by completion time
+    n_train = max(1, int(len(cases) * train_ratio))
+    train_cases = cases[:n_train]
+
+    test_cases_trunc = []
+    for c in cases[n_train:]:
+        seq = c.get("ActTimeSeq", [])
+        case_len = len(seq)
+
+        # If the full case is shorter than the minimum prefix length, ignore it
+        if case_len < MIN_PREFIX_LENGTH:
+            continue
+
+        # Select a random prefix length and truncate test case
+        prefix_len = random.randint(MIN_PREFIX_LENGTH, case_len)
+        truncated_seq = seq[:prefix_len]
+
+        truncated_case = dict(c)
+        truncated_case["ActTimeSeq"] = truncated_seq
+        truncated_case["true_total_time"] = c.get("total_time")
+        truncated_case["total_time"] = "RUNNING"
+        truncated_case["true_total_length"] = case_len
+        test_cases_trunc.append(truncated_case)
+    
+    return train_cases, test_cases_trunc
+
